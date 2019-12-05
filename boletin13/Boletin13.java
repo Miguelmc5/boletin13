e1daa89b6a4c7150ae8e4d7b759470956a431c02
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2019-12-05 12:32:25 +0100

    main

---
+++ boletin13/Boletin13.java
@@ -0,0 +1,24 @@
+package boletin13;
+
+/**
+ *
+ * @author DANIELCASTELAO\mmartinezcosta
+ */
+public class Boletin13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    SeleccionFutbol x1=new Xogador(9, "Lateral D.", 123, "Miguel", "Martinez", 20);
+        System.out.println(x1);
+    SeleccionFutbol a1=new Adestrador("awegfe", 567, "Daniel", "Poceiro", 39);
+        System.out.println(a1);
+    SeleccionFutbol m1=new Masaxista("Grado Universitario", 7, 874, "Brais", "Sanchez", 34);
+        System.out.println(m1);
+    
+    }
+    
+}
